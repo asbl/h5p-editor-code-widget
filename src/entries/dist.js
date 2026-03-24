a671383398cab5f565679de489544e0926c92fc1
Andreas Siebel <andreas.siebel@it-teaching.de>
2026-03-24 21:44:57 +0100

    Revert "Add PythonQuestion editor visibility widget"
    This reverts commit a524b425d7c90761e8fd57a4e4a769282e1a6647.

--- src/entries/dist.js
+++ src/entries/dist.js
@@ -1,8 +1,6 @@
 import CodeWidget from "../scripts/h5peditor-code-widget";
-import PythonQuestionShowWhen from "../scripts/pythonquestion-show-when";
 import "../styles/h5peditor-code-widget.css";
 import CodeWidgetContainer from "../scripts/codewidget-container";
 
 H5PEditor.CodeWidgetContainer = CodeWidgetContainer;
 H5PEditor.widgets.codeWidget = CodeWidget;
-H5PEditor.widgets.pythonQuestionShowWhen = PythonQuestionShowWhen;
